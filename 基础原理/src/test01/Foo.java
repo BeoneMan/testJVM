26b1b56a7bd4407cced012179e7b86abfc054802
qiujiayuan <driveman@qq.com>
2019-11-01 19:17:17 +0800

    添加test01

---
+++ 基础原理/src/test01/Foo.java
@@ -0,0 +1,11 @@
+
+package test01;
+
+public class Foo {
+    public static void main(String[] args) {
+      boolean  boolValue = true; // 将这个true替换为2或者3，再看看打印结果
+        if (true) System.out.println("Hello, Java!");
+        if (boolValue == true) System.out.println("Hello, JVM!");
+    }
+}
+
--- 基础原理/src/test01/Hellow.java
+++
@@ -1,8 +0,0 @@
-package test01;
-
-public class Hellow {
-    public static void main(String[] args) {
-        System.out.println("1234");
-
-    }
-}
